3a2b4e745c053ef3c5d46e4b4f5d79c46a01a5eb
StephFallesen <stephanie1507@live.dk>
2022-04-08 12:26:51 +0200

    nav

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-@keyframe
+//toggle menu
+<script type="text/javascript">
+  var navLinks = document.getElementById("navLinks");
+  function showMenu(){
+    navLinks.style.left = "0"; }
+  function hideMenu(){
+    navLinks.style.left = "-300px"; }
+</script>
