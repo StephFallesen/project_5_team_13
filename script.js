e910008460a95e1ad143a3336f81583cd7b3e259
StephFallesen <stephanie1507@live.dk>
2022-04-11 17:01:26 +0200

    3 tekstbokse lavet

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //toggle menu
 <script type="text/javascript">
-  var navLinks = document.getElementById("navLinks");
-  function showMenu(){
-    navLinks.style.left = "0"; }
-  function hideMenu(){
-    navLinks.style.left = "-300px"; }
+  var navLinks = document.getElementById("nav-links");
+  function showMenu()
+    navLinks.style.left = "0";
+  function hideMenu()
+    navLinks.style.left = "-300px";
 </script>
